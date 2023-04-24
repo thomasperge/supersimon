8d5a2728be059e986dc76b667aaa6ec18f90ac09
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-24 09:57:49 +0200

    feat: set defaut cookie

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
+const getCookieValue = (name) => (
+    document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
+)
+  
 document.addEventListener('DOMContentLoaded', function() {
-}
+    // Set default cookie
+    if (!getCookieValue("gameStarted") || getCookieValue("gameStarted") == undefined) {
+        document.cookie = "gameStarted=false"
+    }
+})
