ed9fb3bcb740ba62750e65669fb564bce7537ec4
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-24 10:31:09 +0200

    feat: add start page loading screen

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ const getCookieValue = (name) => (
     document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
 )
   
+// Start the game
 document.addEventListener('DOMContentLoaded', function() {
     // Set default cookie
-    if (!getCookieValue("gameStarted") || getCookieValue("gameStarted") == undefined) {
+    if (!getCookieValue("gameStarted") || getCookieValue("gameStarted") == undefined || getCookieValue("gameStarted") == "false") {
         document.cookie = "gameStarted=false"
+        document.querySelector('.container').style = "filter: blur(3.5px);"
+    } else {
+        document.cookie = "gameStarted=false"
+        document.querySelector('.container').style = "filter: blur(3.5px);"
     }
+})
+
+document.getElementById('start').addEventListener('click', () => {
+    document.cookie = "gameStarted=true"
+    document.querySelector('.container').style = "filter: blur(0px);"
+    document.querySelector('.waitingScreenContainer').style = "display: none"
 })
