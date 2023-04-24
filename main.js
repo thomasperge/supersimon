9b73a13981ba0dc1b590f3d43e9136956a1f8275
Thomasperge <thomaskauffmant74@gmail.com>
2023-04-24 11:05:48 +0200

    feat: add round + game loop

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let gameStarted = false
+
 const getCookieValue = (name) => (
     document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || ''
 )
@@ -14,8 +16,40 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 })
 
+// Event when click => "Start the game"
 document.getElementById('start').addEventListener('click', () => {
+    // Delete waiting screen
     document.cookie = "gameStarted=true"
     document.querySelector('.container').style = "filter: blur(0px);"
     document.querySelector('.waitingScreenContainer').style = "display: none"
+
+    // Set Timer
+    let temps = 0
+    const timerElement = document.getElementById("timer")
+
+    function diminuerTemps() {
+        timerElement.innerText = `${temps}s`
+        temps++
+    }
+
+    setInterval(diminuerTemps, 1000)
+
+    gameStarted = true
 })
+
+// Game
+let roundCount = 0
+let roundColorGenerate = []
+let roundColorPlayer = []
+
+function loop() {
+    if (getCookieValue("gameStarted") == "true" && roundCount >= 0) {
+        // Afficher le banière round pendant 2s
+        // Afficher les differentes couleurs pendant 1.5s chacune
+        // Le player met les couelies
+    }
+    
+    window.requestAnimationFrame(loop)
+}
+
+window.requestAnimationFrame(loop)
